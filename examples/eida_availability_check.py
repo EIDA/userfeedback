40caaddee92c3613c6a2fdd0e0452387d16e7654
Javier Quinteros <javier@quinteros.eu>
2020-01-10 12:41:04 +0100

    Use module os to work with paths

--- examples/eida_availability_check.py
+++ examples/eida_availability_check.py
@@ -1,7 +1,7 @@
 from obspy import Inventory, UTCDateTime
 import json, urllib.request
 from obspy.clients.fdsn import Client
-import xml.etree.ElementTree as ET
+import os
 
 debug = False
 channel = 'HHZ'
@@ -9,7 +9,7 @@ location = '*'
 years = ['2018']
 include_restricted = True
 
-token = '/home/fuchs/.eidatoken'
+token = os.path.expanduser('~/.eidatoken') # path to personal eida token here
 
 nodes = [{'name': 'ODC', 'stationurl': 'http://www.orfeus-eu.org/fdsnws/station/1/query?', 'dataurl': 'http://www.orfeus-eu.org/fdsnws/dataselect/1/query?', 'wfcurl': 'http://www.orfeus-eu.org/eidaws/wfcatalog/1/query?'},
 		 {'name': 'ETH', 'stationurl': 'http://eida.ethz.ch/fdsnws/station/1/query?', 'dataurl': 'http://eida.ethz.ch/fdsnws/dataselect/1/query?', 'wfcurl': 'http://eida.ethz.ch/eidaws/wfcatalog/1/query?'},
@@ -21,9 +21,6 @@ nodes = [{'name': 'ODC', 'stationurl': 'http://www.orfeus-eu.org/fdsnws/station/
 		 {'name': 'NOA', 'stationurl': 'http://eida.gein.noa.gr/fdsnws/station/1/query?', 'dataurl': 'http://eida.gein.noa.gr/fdsnws/dataselect/1/query?', 'wfcurl': 'http://eida.gein.noa.gr/eidaws/wfcatalog/1/query?'},
 		 {'name': 'NIEP', 'stationurl': 'http://eida-sc3.infp.ro/fdsnws/station/1/query?', 'dataurl': 'http://eida-sc3.infp.ro/fdsnws/dataselect/1/query?', 'wfcurl': 'http://geofon.gfz-potsdam.de/eidaws/wfcatalog/1/query?'},
 		 {'name': 'KOERI', 'stationurl': 'http://eida-service.koeri.boun.edu.tr/fdsnws/station/1/query?', 'dataurl': 'http://eida-service.koeri.boun.edu.tr/fdsnws/dataselect/1/query?', 'wfcurl': 'http://eida-service.koeri.boun.edu.tr/eidaws/wfcatalog/1/query?'}]
-
-
-
 
 print('-----------------------------------------------------')
 for year in years :
@@ -55,33 +52,32 @@ for year in years :
 			for station in network :
 				try :
 					metrics = json.loads(urllib.request.urlopen(url + 'network=' + network.code + '&station=' + station.code + '&channel=' + channel + '&location=' + location + '&include=sample&start=' + y1 + '-' + m1 + '-' + d1 + 'T00:00:00.0000&end=' + y2 + '-' + m2 + '-' + d2 + 'T23:59:59.9999&longestonly=false&minimumlength=0.0').read())
-					success = 'yes'
 					#print 'Retrieved metrics for', network.code, station.code
 				except Exception as e :
-					success = 'no'
-					print('! NO ! metrics for', year, network.code, station.code)
-					resultlist.append([year, node['name'], network.code, station.code, -1])
-					nowfc_list.append([year, node['name'], network.code, station.code])
-				if success == 'yes' :
-					temp = 0
-					for entry in metrics :
-						temp += entry['percent_availability']
-					if station.end_date == None and station.termination_date == None :
-						availability = temp / 365.0
-					else :
-						availability = temp / (station.end_date.julday)
-					print(year, node['name'], network.code, station.code, availability)
-					resultlist.append([year, node['name'], network.code, station.code, availability])
-	print('-----------------------------------------------------')
+                    print('! NO ! metrics for', year, network.code, station.code)
+                    resultlist.append([year, node['name'], network.code, station.code, -1])
+                    nowfc_list.append([year, node['name'], network.code, station.code])
+                    continue
 
-	f = open('eida_availability_results_' + year + '_' + channel + '.txt','w+')
-	for line in resultlist :
-		to_write = line[0] + '    ' + line[1] + '    ' + line[2].rjust(5, ' ') + '    ' + line[3].rjust(5, ' ') + '    ' + str(line[4])
-		f.write(to_write + '\n')
-	f.close()
+                temp = 0
+                for entry in metrics :
+                    temp += entry['percent_availability']
+                if station.end_date == None and station.termination_date == None :
+                    availability = temp / 365.0
+                else :
+                    availability = temp / (station.end_date.julday)
+                print(year, node['name'], network.code, station.code, availability)
+                resultlist.append([year, node['name'], network.code, station.code, availability])
+    print('-----------------------------------------------------')
 
-	fn = open('eida_nowfc_' + year + '_' + channel + '.txt','w+')
-	for line in nowfc_list :
-		to_write = line[0] + '    ' + line[1] + '    ' + line[2].rjust(5, ' ') + '    ' + line[3].rjust(5, ' ')
-		fn.write(to_write + '\n')
-	fn.close()
+    f = open('eida_availability_results_' + year + '_' + channel + '.txt','w+')
+    for line in resultlist :
+        to_write = line[0] + '    ' + line[1] + '    ' + line[2].rjust(5, ' ') + '    ' + line[3].rjust(5, ' ') + '    ' + str(line[4])
+        f.write(to_write + '\n')
+    f.close()
+
+    fn = open('eida_nowfc_' + year + '_' + channel + '.txt','w+')
+    for line in nowfc_list :
+        to_write = line[0] + '    ' + line[1] + '    ' + line[2].rjust(5, ' ') + '    ' + line[3].rjust(5, ' ')
+        fn.write(to_write + '\n')
+    fn.close()
